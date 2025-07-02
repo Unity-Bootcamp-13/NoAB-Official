dd75f73af1fc81362893f2fac7857704676c8a50
jinheehan829 <1022jinhee@gmail.com>
2025-07-02 14:08:42 +0900

    Feat : 스킬 시스템 로직 개요

---
+++ OverwatchDefenseMobile/Assets/Scripts/Player/Cassidy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Cassidy : MonoBehaviour
+{
+
+    private Skill normalAttack;
+
+    public Projectile attackProjectile;
+    public Projectile skill01Projectile;
+    public Projectile skill02Projectile;
+    public Projectile ultimateProjectile;
+
+    void Start()
+    {
+        normalAttack = new Skill(SkillType.Attack, CastType.Auto, RequirementType.Cooltime, attackProjectile);
+        normalAttack.action += NormalAttack;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            normalAttack.Activate();
+    }
+
+    private void NormalAttack()
+    {
+        Debug.Log("ATTACK");
+
+    }
+}
