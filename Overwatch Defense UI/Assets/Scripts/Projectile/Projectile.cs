07ff274011d757756f99d04d4caf8d5aaa02a2c9
todays-sun-day <178suna@gmail.com>
2025-07-03 11:35:04 +0900

    Refactor: 투사체 관련 스크립트 수정
    1. projectile.cs 내 ProjectileSettings 를 ProjectileManager 로 이동
    2. ProjectileManager 내 캐서디의 투사체 스킬 세팅을 캐서디로 옮김 (삭제함)
    3. 투사체의 _poolsByPrefab 을 삭제

--- Overwatch Defense UI/Assets/Scripts/Projectile/Projectile.cs
+++ Overwatch Defense UI/Assets/Scripts/Projectile/Projectile.cs
@@ -1,42 +1,56 @@
 using UnityEngine;
 
-public struct ProjectileSettings
-{
-    public string name;
-    public float speed;
-    public float lifetime;
-    public int damage;
-    public bool useGravity;
-    public CollisionDetectionMode collisionDetectionMode;
-}
 
-[RequireComponent(typeof(Rigidbody),typeof(Collider))]
+[RequireComponent(typeof(Rigidbody), typeof(Collider))]
 public class Projectile : MonoBehaviour
 {
-    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] Rigidbody _rigidbody;
+    [SerializeField] Collider _collider;
+    private int _id;
     private float _lifetime;
     private int _damage;
+    private float _spawnTime;
+
+    public int Id { get { return _id; } }
 
     private ProjectilePoolManager _projectilePoolManager;
 
-    /// <summary>
-    /// 
-    /// </summary>
     public void Initialize(Vector3 position, Vector3 direction, ProjectileSettings projectileSettings)
     {
         transform.position = position;
         transform.rotation = Quaternion.LookRotation(direction);
 
+        _id = projectileSettings.id;
         _lifetime = projectileSettings.lifetime;
         _damage = projectileSettings.damage;
+        _spawnTime = Time.time;
 
-        rigidbody.useGravity = projectileSettings.useGravity;
-        rigidbody.collisionDetectionMode = projectileSettings.collisionDetectionMode;
-        rigidbody.linearVelocity = direction * projectileSettings.speed;
+        _rigidbody.useGravity = projectileSettings.useGravity;
+        _rigidbody.collisionDetectionMode = projectileSettings.collisionDetectionMode;
+        _rigidbody.linearVelocity = direction * projectileSettings.speed;
+
+        _collider.isTrigger = true;
     }
 
     public void InjectPoolManager(ProjectilePoolManager projectilePoolManager)
     {
         _projectilePoolManager = projectilePoolManager;
+    }
+
+    private void Update()
+    {
+        if (Time.time - _spawnTime > _lifetime)
+        {
+            _projectilePoolManager.ReturnProjectile(this);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Zomnic"))
+        {
+            Zomnic zomnic = other.GetComponent<Zomnic>();
+            zomnic.TakeDamage(_damage);
+        }
     }
 }
